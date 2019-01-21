0c73e3967e47cf8772c7c199d7d466ba89b8eafa
Master Fox <xavierwere@gmail.com>
2019-01-21 21:12:05 +0300

    Finishing touches

---
+++ pkg/list/list.go
@@ -0,0 +1,50 @@
+package list
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/masterxavierfox/logwatch/pkg/cloudwatchlogs"
+)
+
+type List struct {
+	GroupName  string
+	MaxResults int
+}
+
+func (l List) Run() {
+	if len(l.GroupName) > 0 {
+		l.listLogStreams()
+	} else {
+		l.listLogGroups()
+	}
+}
+
+func (l List) listLogGroups() {
+	client := cloudwatchlogs.NewClient()
+	groupNames, err := client.GetLogGroupNames(l.MaxResults)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, groupName := range groupNames {
+		fmt.Println(groupName)
+	}
+}
+
+func (l List) listLogStreams() {
+	client := cloudwatchlogs.NewClient()
+	streams, err := client.GetLogStreamNames(l.GroupName, l.MaxResults)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, stream := range streams {
+		lastEvent := time.Now().Sub(stream.LastEvent).Truncate(time.Second)
+
+		fmt.Printf("%s (%s)\n", stream.Name, lastEvent)
+	}
+}
